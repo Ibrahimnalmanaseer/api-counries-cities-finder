8f1d2220815731ed3716302f2e0827481cdb6c4f
Ibrahim Almanaseer <ibrahimnalmanaseer@gmail.com>
2022-10-31 21:23:54 +0000

    feat :add all the required

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -17,7 +17,7 @@ class handler(BaseHTTPRequestHandler):
         url_components=parse.urlsplit(api_path)
         query=parse.parse_qsl(url_components.query)
         dic_set=dict(query)
-        self.wfile.write(str(dic_set).encode())
+       
        
 
         # if dic_set['country']:
@@ -33,25 +33,25 @@ class handler(BaseHTTPRequestHandler):
         # else:
 
         #     self.wfile.write(str(f'').encode())
-        if 'country' in dic_set:
-            url='https://restcountries.com/v3.1/name/'
-            country=dic_set['country']
-            r = requests.get(url + country)
-            data = r.json()
-            capital=data[0]['capital'][0]
-            message=f"The capital of {country} is {capital}"
-        elif 'capital' in dic_set:
-            url='https://restcountries.com/v3.1/capital/'
-            capital=dic_set['capital']
-            r = requests.get(url + capital)
-            data = r.json()
-            country= data[0]['name']['common']
-            message=f"{capital} is the capital of {country}."
+        # if 'country' in dic_set:
+        #     url='https://restcountries.com/v3.1/name/'
+        #     country=dic_set['country']
+        #     r = requests.get(url + country)
+        #     data = r.json()
+        #     capital=data[0]['capital'][0]
+        #     message=f"The capital of {country} is {capital}"
+        # elif 'capital' in dic_set:
+        #     url='https://restcountries.com/v3.1/capital/'
+        #     capital=dic_set['capital']
+        #     r = requests.get(url + capital)
+        #     data = r.json()
+        #     country= data[0]['name']['common']
+        #     message=f"{capital} is the capital of {country}."
 
-        else:
-            message = " no result was found , please try again "
+        # else:
+        #     message = " no result was found , please try again "
        
 
-        self.wfile.write(message.encode())
+        self.wfile.write(str('done').encode())
 
         return
