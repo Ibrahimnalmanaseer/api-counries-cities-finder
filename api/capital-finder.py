0f3b45807da0870bd1744f0cd30c9d63123b5a74
Ibrahim Almanaseer <ibrahimnalmanaseer@gmail.com>
2022-10-31 21:33:21 +0000

    feat add all the required and improve the code functionality

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -1,7 +1,5 @@
 
 from http.server import BaseHTTPRequestHandler
-
-
 from urllib import parse
 from webbrowser import get
 import requests
@@ -9,6 +7,11 @@ import requests
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+
+        '''
+        display the country name or a capital details depends on user query
+        
+        '''
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
@@ -20,38 +23,31 @@ class handler(BaseHTTPRequestHandler):
        
        
 
-        # if dic_set['country']:
-        #     country=dic_set['country']
-        #     r=requests(url_country+country)
-        #     data=r.json()
-        #     capital=data[0]['capital'][0]
-            
-            # self.wfile.write(str(f'capital of {country}is {capital}').encode())
-        
-        # elif dic_set['capital']:
-                
-        # else:
+ 
+        if dic_set['country']:
 
-        #     self.wfile.write(str(f'').encode())
-        # if 'country' in dic_set:
-        #     url='https://restcountries.com/v3.1/name/'
-        #     country=dic_set['country']
-        #     r = requests.get(url + country)
-        #     data = r.json()
-        #     capital=data[0]['capital'][0]
-        #     message=f"The capital of {country} is {capital}"
-        # elif 'capital' in dic_set:
-        #     url='https://restcountries.com/v3.1/capital/'
-        #     capital=dic_set['capital']
-        #     r = requests.get(url + capital)
-        #     data = r.json()
-        #     country= data[0]['name']['common']
-        #     message=f"{capital} is the capital of {country}."
+            url='https://restcountries.com/v3.1/name/'
+            country=dic_set['country']
 
-        # else:
-        #     message = " no result was found , please try again "
+            r = requests.get(url + country)
+            data = r.json()
+            capital=data[0]['capital'][0]
+
+            display=f"The capital of {country} is {capital}"
+
+
+        elif 'capital' in dic_set:
+            url='https://restcountries.com/v3.1/capital/'
+            capital=dic_set['capital']
+            r = requests.get(url + capital)
+            data = r.json()
+            country= data[0]['name']['common']
+            display=f"{capital} is the capital of {country}."
+
+        else:
+            display = " no result was found , please try again "
        
 
-        self.wfile.write(str('done').encode())
+        self.wfile.write(str(display).encode())
 
         return
