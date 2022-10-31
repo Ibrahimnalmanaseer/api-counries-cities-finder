799f67e5e8185d3c11032d20c37595b33ff96227
Ibrahim Almanaseer <ibrahimnalmanaseer@gmail.com>
2022-10-31 20:53:07 +0000

    f

--- api/main.py
+++ api/main.py
@@ -1,18 +1,17 @@
 
-# from http.server import BaseHTTPRequestHandler
-# from datetime import datetime
+from http.server import BaseHTTPRequestHandler
+from datetime import datetime
 
-# class handler(BaseHTTPRequestHandler):
+class handler(BaseHTTPRequestHandler):
 
-#     def do_GET(self):
-#         self.send_response(200)
-#         self.send_header('Content-type','text/plain')
-#         self.end_headers()
-#         date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         
-#         self.wfile.write(str(date).encode())
-#         return
+        self.wfile.write(str(date).encode())
+        return
 
-dic_set=dict([('name', 'amman')])
 
 
